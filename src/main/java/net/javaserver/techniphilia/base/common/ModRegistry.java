b842c8c816feb9aa4848e5c800b502c0a7105c85
TeraV <vharris1800@gmail.com>
2021-09-12 16:08:06 -0400

    Added preliminary version info for 0.0.1.0

--- src/main/java/net/javaserver/techniphilia/base/common/ModRegistry.java
+++ src/main/java/net/javaserver/techniphilia/base/common/ModRegistry.java
@@ -3,6 +3,6 @@ package net.javaserver.techniphilia.base.common;
 public class ModRegistry {
     public static final String MOD_ID = "techniphilia-base";
     public static final String MOD_NAME = "Techniphilia Base";
-    public static final String MOD_VERSION = "0.0.0.0";
+    public static final String MOD_VERSION = "0.0.1.0";
     public static final String MOD_DEPENDENCIES = "required-after:techniphilia-core@[0.5.10.5,)";
 }

72b320f8329172a9bbdad9782168d96502fb074b
TeraV <vharris1800@gmail.com>
2021-09-12 16:12:50 -0400

    Updated ModRegistry to use new Core-parallel Logger approach

--- src/main/java/net/javaserver/techniphilia/base/ComponentBaseLoader.java
+++ src/main/java/net/javaserver/techniphilia/base/ComponentBaseLoader.java
@@ -5,23 +5,20 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ModRegistry.MOD_ID,
         name = ModRegistry.MOD_NAME,
         version = ModRegistry.MOD_VERSION,
         dependencies = ModRegistry.MOD_DEPENDENCIES)
 public class ComponentBaseLoader {
-    private static Logger logger;
-
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        logger = event.getModLog();
-        logger.info("TechniphiliaBase: preInit");
+        ModRegistry.logger = event.getModLog();
+        ModRegistry.logger.info("TechniphiliaBase: preInit");
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        logger.info("Hello blocky world!");
+        ModRegistry.logger.info("Hello blocky world!");
     }
 }

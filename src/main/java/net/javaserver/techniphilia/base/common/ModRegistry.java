beb38252ca26cb4b88e563d5eab92cf89ae76fc1
TeraV <vharris1800@gmail.com>
2021-09-12 16:21:02 -0400

    Updated ModRegistry to include ModItemGroup synonymous with Core

--- src/main/java/net/javaserver/techniphilia/base/common/ModRegistry.java
+++ src/main/java/net/javaserver/techniphilia/base/common/ModRegistry.java
@@ -1,5 +1,7 @@
 package net.javaserver.techniphilia.base.common;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import org.apache.logging.log4j.Logger;
 
 public class ModRegistry {
@@ -8,4 +10,21 @@ public class ModRegistry {
     public static final String MOD_VERSION = "0.0.1.0";
     public static final String MOD_DEPENDENCIES = "required-after:techniphilia-core@[0.5.10.5,)";
     public static Logger logger;
+    private static ModItemGroup itemGroup = null;
+    private ModRegistry() {}
+    public static CreativeTabs getItemGroup() {
+        if (itemGroup == null) {
+            itemGroup = new ModItemGroup();
+        }
+        return itemGroup;
+    }
+    private static class ModItemGroup extends CreativeTabs {
+        private ModItemGroup() {
+            super("techniphiliaBaseBase");
+        }
+        @Override
+        public ItemStack getTabIconItem() {
+            return ItemStack.EMPTY;
+        }
+    }
 }
